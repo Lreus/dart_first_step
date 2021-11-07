2cf9b92e87313fd5f224b71fa72debc1926efa95
Lreus <ludovic.reus@free.fr>
2021-11-07 22:15:10 +0100

    adding queens to board

--- bin/eight_queen_puzzle_test.dart
+++ bin/eight_queen_puzzle_test.dart
@@ -23,14 +23,45 @@ void main() {
       expect(() => puzzle.addQueen(pos[0], pos[1]), throwsArgumentError);
     }
   });
+
+  test('A queen placed has a row and a column', () {
+    puzzle.addQueen(2, 5);
+    var expectedQueen = puzzle.queens[0];
+    expect(expectedQueen.row, equals(2));
+    expect(expectedQueen.column, equals(5));
+  });
+
+  test('placing two queens on same position is forbidden', () {
+    const int row = 1;
+    const int column = 1;
+
+    puzzle.addQueen(row, column);
+    expect(() => puzzle.addQueen(row, column), throwsA(isA<ArgumentError>()));
+  });
 }
 
 class Puzzle {
+  List<Queen> queens = [];
+
   bool isSolved() { return false; }
 
   void addQueen(int line, int column) {
     if (line <= 0 || line > 8 || column <= 0 || column > 8) {
       throw ArgumentError();
     }
+
+    for (final Queen queen in queens) {
+      if (line == queen.row && column == queen.column) {
+        throw ArgumentError();
+      }
+    }
+    queens.add(Queen(line, column));
   }
+}
+
+class Queen {
+  int row;
+  int column;
+
+  Queen(this.row,this.column);
 }
